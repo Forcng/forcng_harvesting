04f100e733d2ec426d6b65164151b4b48a8e344f
Forcng <164341281+Forcng@users.noreply.github.com>
2025-08-01 05:51:23 -0400

    /

--- server/server.lua
+++ server/server.lua
@@ -1,15 +1,14 @@
 local ESX = exports['es_extended']:getSharedObject()
-
-local lastHarvestStart = {}
+local lastHarvest = {}
 
 AddEventHandler('playerDropped', function()
-    lastHarvestStart[source] = nil
+    lastHarvest[source] = nil
 end)
 
 RegisterNetEvent("forcng:harvestStart", function(zoneId)
     local src = source
     if not Config.HarvestingLocations[zoneId] then return end
-    lastHarvestStart[src] = { t = os.time(), zoneId = zoneId }
+    lastHarvest[src] = { t = os.time(), zoneId = zoneId }
 end)
 
 RegisterNetEvent("forcng:harvest", function(zoneId)
@@ -17,12 +16,12 @@ RegisterNetEvent("forcng:harvest", function(zoneId)
     local xPlayer = ESX.GetPlayerFromId(src)
     if not xPlayer then return end
 
-    local start = lastHarvestStart[src]
+    local start = lastHarvest[src]
     if not start or start.zoneId ~= zoneId or (os.time() - start.t) > 20 then
         DropPlayer(src, "WARNING: Exploit detected.")
         return
     end
-    lastHarvestStart[src] = nil
+    lastHarvest[src] = nil
 
     local zone = Config.HarvestingLocations[zoneId]
     if not zone then return end
